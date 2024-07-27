7485734f078a724ec15e5acce383f0dcb1ec0461
waasimAnsari786 <waasimansari92@gmail.com>
2024-07-27 12:14:39 -0700

    I created this Weather App from React.JS.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import WhrApp from "./components/WhrApp.jsx";
+
+function App() {
+  return (
+    <>
+      <WhrApp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/WhrApp.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+export default function WhrApp() {
+  const [inpVal, setInpVal] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [APIData, setAPIData] = useState();
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inpVal}&appid=e8b2a8e8dfc6de36a2ca5b889ba4bfa2`;
+
+  const handleOnChange = (e) => {
+    setInpVal(e.target.value);
+  };
+
+  const kelvinToCelcies = (kelvin) => {
+    return (kelvin - 273.15).toFixed(2);
+  };
+
+  const getData = async () => {
+    setCityName(inpVal);
+    const resp = await fetch(URL);
+    const data = await resp.json();
+    setAPIData(data);
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="container d-flex justify-content-center align-items-center flex-column mt-5">
+        <h1 className="heading mb-5">react wheather app</h1>
+        <div className="col-6 w-app d-flex justify-content-center align-items-center flex-column">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              getData();
+            }}
+          >
+            <input
+              onChange={handleOnChange}
+              className="search-bar"
+              type="text"
+              name="search"
+              placeholder="search"
+            />
+          </form>
+
+          <h1 className="mt-5">{cityName}</h1>
+          {APIData ? (
+            <>
+              <h3>{kelvinToCelcies(APIData.main.temp)}&deg;C</h3>
+              <span>{kelvinToCelcies(APIData.main.temp_max)}&deg;C</span>
+              <span>{kelvinToCelcies(APIData.main.temp_min)}&deg;C</span>
+            </>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
